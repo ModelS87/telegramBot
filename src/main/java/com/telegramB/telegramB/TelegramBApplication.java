3a706297d80c7ae1ae5519d976e3c05108c71d8a
Александра <bzzz18@yandex.ru>
2023-08-15 17:24:26 +0500

    check

---
+++ src/main/java/com/telegramB/telegramB/TelegramBApplication.java
@@ -0,0 +1,13 @@
+package com.telegramB.telegramB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBApplication.class, args);
+	}
+
+}
